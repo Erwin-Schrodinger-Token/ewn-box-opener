b311afaad7c30f0c86a591d36f2b6257d8e2a9bf
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2024-09-26 10:19:58 +0100

    Add box opener script

---
+++ run_box_opener.py
@@ -0,0 +1,61 @@
+import logging
+import time
+import os
+import requests
+
+from pybip39 import Mnemonic
+
+
+logging.basicConfig(level=logging.INFO)
+API_KEY = os.environ.get("API_KEY")
+API_URL = os.environ.get("API_URL", "https://api.erwin.lol")
+
+
+def submit_guesses():
+    passwords = []
+
+    for x in range(0, 50):
+        passwords.append(Mnemonic().phrase)
+
+    logging.info("🔑️ Generated %s guesses" % len(passwords))
+    logging.info("➡️ Submitting to oracle")
+
+    url = '%s/submit_guesses' % API_URL
+    headers = {
+        'x-api-key': API_KEY,
+        'content-type': 'application/json'
+    }
+    resp = requests.post(
+        url,
+        json=passwords,
+        headers=headers,
+        timeout=60
+    )
+
+    if resp.status_code == 202:
+        logging.info("✅ Guesses accepted")
+        return False
+    else:
+        logging.info("❌ Guesses rejected (%s): %s" % (resp.status_code, resp.text))
+        return True
+
+
+def do_loop():
+    sleep_time = 10
+    while True:
+        logging.info("⚙️ Generating guesses")
+        try:
+            rate_limited = submit_guesses()
+            if rate_limited:
+                sleep_time += 10
+            else: sleep_time -= 1
+        except Exception as err:
+            logging.error("⚠️ Error occurred: %s" % str(err))
+        time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    if not API_KEY:
+        logging.error("⚠️ API Key not defined, set API_KEY environment variable")
+    else:
+        do_loop()
